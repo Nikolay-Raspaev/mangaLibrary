f3f1920485dd8c1cedef6e9bd7ebbb4ff661959f
Николай <raspaev.niko@mail.ru>
2023-02-13 19:47:48 +0400

    Готовая

--- src/main/java/com/LabWork/app/AppApplication.java
+++ src/main/java/com/LabWork/app/AppApplication.java
@@ -2,7 +2,6 @@ package com.LabWork.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,34 +15,29 @@ public class AppApplication {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
-	@CrossOrigin
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
 
-	@CrossOrigin
 	@GetMapping("/sum")
 	public String Sum(@RequestParam(required = false, defaultValue = "0") double first,
 					  @RequestParam(required = false, defaultValue = "0") double second) {
 		return Double.toString(first + second);
 	}
 
-	@CrossOrigin
 	@GetMapping("/difference")
 	public String Difference(@RequestParam(required = false, defaultValue = "0") double first,
 					  @RequestParam(required = false, defaultValue = "0") double second) {
 		return Double.toString(first - second);
 	}
 
-	@CrossOrigin
 	@GetMapping("/multiplication")
 	public String Multiplication(@RequestParam(required = false, defaultValue = "1") double first,
 					   @RequestParam(required = false, defaultValue = "1") double second) {
 		return Double.toString(first * second);
 	}
 
-	@CrossOrigin
 	@GetMapping("/division")
 	public String Division(@RequestParam(required = false, defaultValue = "1") double first,
 					  @RequestParam(required = false, defaultValue = "1") double second) {
@@ -53,4 +47,10 @@ public class AppApplication {
 		}
 		return Double.toString(first/second);
 	}
+
+	@GetMapping("/binary")
+	public String Binary(@RequestParam int result) {
+		return Double.toString(result);
+	}
+
 }
