ba86843ae3fdaf9fae19101ec0c47ade3aa3ec5a
Николай <raspaev.niko@mail.ru>
2023-04-30 23:37:07 +0400

    Можно работать

--- front/src/Dto/Manga-Dto.js
+++
@@ -1,9 +0,0 @@
-export default class MangaDto {
-    constructor(args) {
-        this.id = args.id || null;
-        this.creatorId = args.creatorId || 0;
-        this.mangaName = args.mangaName || "";
-        this.image = args.image;
-        this.chapterCount = args.chapterCount || 0;
-    }
-}
